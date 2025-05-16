a956b87d0af6685041d6840c269d829d15118ed4
Vedant Nanoti <167440282+vedantnanoti@users.noreply.github.com>
2025-05-16 21:27:55 +0530

    Update face_detection_using_image.py

--- face_detection_using_image.py
+++ face_detection_using_image.py
@@ -4,11 +4,10 @@ import os
 
 # Importing the haarcascades folder from the OpenCV library for face detection using a pre-trained model.
 # The os.listdir() function converts all the available folders in cv2 into a list format.
-# The Haarcascade file is trained for face detection.
 print(os.listdir(cv2.data.haarcascades))
 filename = "haarcascade_frontalface_default.xml"
 
-# Finding the path of the haarcascade file from the print statement above.
+# creating a path for our souce file using the specific haarcascade file from the print statement above.
 # After finding the path, we append our source file to that path and store it in the 'path' variable.
 print(cv2.data.haarcascades)
 path = cv2.data.haarcascades + filename
@@ -19,7 +18,7 @@ image = cv2.imread("photo.jpg")
 # Using the CascadeClassifier to detect faces in the image using the source path.
 # After detecting the face, it scales the image by 30% for 5 times to check if the face remains consistent.
 # If a face is not detected in any of the 5 attempts, it will discard that detection.
-# The function also returns the coordinates of the detected faces.
+# The print statement returns the coordinates of the detected faces form the detectMultiScale().
 model = cv2.CascadeClassifier(path)
 faces = model.detectMultiScale(image, 1.3, 5)
 print(faces)
@@ -33,8 +32,7 @@ for face in faces:
     y2 = y1 + face[3]
     cv2.rectangle(image, (x1, y1), (x2, y2), [0, 0, 0], 3)
 
-# Converting the image into a 3D array in BGR format.
-# After line 25, we have successfully created a box around the face and now display the image using imshow().
+# After line 32, we have successfully created a box around the face and now display the image using imshow().
 cv2.imshow("me", image)
 
 # Freezing the window until a key is pressed
